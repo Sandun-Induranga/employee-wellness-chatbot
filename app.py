11461263094ab47452edc19e1e5bff8de9ccb094
Sandun Induranga <sandutharu40@gmail.com>
2024-09-14 11:00:53 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+from chat import get_response
+
+app = FastAPI()
+
+class Message(BaseModel):
+    msg: str
+
+origins = ["*"]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/chat/")
+async def create_item(body : Message):
+    reply = get_response(body.msg);
+    return {"reply":reply};
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
